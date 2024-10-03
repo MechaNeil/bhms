9f633fa0badfdab04f0b21e2f96c57d686191b2e
MechaNeil <markneilteves12345@gmail.com>
2024-10-03 19:49:35 +0800

    Update tenants-requests.blade.php

--- resources/views/livewire/roles/owner/pages/notification/requests/tenants-requests.blade.php
+++ resources/views/livewire/roles/owner/pages/notification/requests/tenants-requests.blade.php
@@ -47,70 +47,68 @@
                                     <div class="card mb-4">
                                         <div class="card-header">
     
-                                            <h3 class="card-title">Beds Table</h3>
+                                            <h3 class="card-title">Tenants Requests</h3>
     
     
                                         </div>
                                         <!-- /.card-header -->
                                         <div class="card-body">
     
-                                            <table class="table table-bordered">
-                                                <thead class="table-mode">
-                                                    <tr>
-                                                        <th>Tenant Name</th>
-                                                        <th>Requests</th>
-                                                        <th>Date Issued</th>
-                                                        <th>Reply from owner</th>
-    
-                                                        <th style="width: 100px" class=" text-center">Status</th>
-                                                                                                                <th style="width: 109px" class=" text-center">Action</th>
-
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr class="align-middle">
-                                                        <td>Neil Tevs</td>
-                                                        <td>Repair Bed</td>
-                                                        <td>September 28, 2024</td>
-                                                        <td>
-                                                            
-                                                        </td>
-                                                        <td class="text-center">
-                                                            <span
-                                                                class="badge rounded-pill text-bg-success">pending</span>
-                                                        </td>
-                                                        <td class=" text-center">
-                                                            <button class="btn btn-primary btn-sm m-1" type="button">
-                                                                <i class="bi bi-eye-fill"></i>
-                                                            </button>
-                                                            <button class="btn btn-success btn-sm m-1" type="button">
-                                                                <i class="bi bi-reply-fill"></i>
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr class="align-middle">
-                                                        <td>Mark Tevs</td>
-                                                        <td>No wifi connection</td>
-                                                        <td>September 28, 2024</td>
-                                                        <td>
-                                                            Noted
-                                                        </td>
-                                                        <td class=" text-center">
-                                                            <span class="badge rounded-pill text-bg-success">solve</span>
-                                                        </td>
-                                                        <td class="text-center">
-                                                            <button class="btn btn-primary btn-sm m-1" type="button">
-                                                                <i class="bi bi-eye-fill"></i>
-                                                            </button>
-                                                            <button class="btn btn-success btn-sm m-1" type="button">
-                                                                <i class="bi bi-reply-fill"></i>
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-    
-    
-                                                </tbody>
-                                            </table>
+                                            <div class="table-responsive">
+                                                <table class="table table-bordered">
+                                                    <thead class="table-mode">
+                                                        <tr>
+                                                            <th>Tenant Name</th>
+                                                            <th>Requests</th>
+                                                            <th>Date Issued</th>
+                                                            <th>Reply from owner</th>
+                                                            <th style="width: 100px" class=" text-center">Status</th>
+                                                                                                                    <th style="width: 109px" class=" text-center">Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr class="align-middle">
+                                                            <td>Neil Tevs</td>
+                                                            <td>Repair Bed</td>
+                                                            <td>September 28, 2024</td>
+                                                            <td>
+                                                
+                                                            </td>
+                                                            <td class="text-center">
+                                                                <span
+                                                                    class="badge rounded-pill text-bg-success">pending</span>
+                                                            </td>
+                                                            <td class=" text-center">
+                                                                <button class="btn btn-primary btn-sm m-1" type="button">
+                                                                    <i class="bi bi-eye-fill"></i>
+                                                                </button>
+                                                                <button class="btn btn-success btn-sm m-1" type="button">
+                                                                    <i class="bi bi-reply-fill"></i>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                        <tr class="align-middle">
+                                                            <td>Mark Tevs</td>
+                                                            <td>No wifi connection</td>
+                                                            <td>September 28, 2024</td>
+                                                            <td>
+                                                                Noted
+                                                            </td>
+                                                            <td class=" text-center">
+                                                                <span class="badge rounded-pill text-bg-success">solve</span>
+                                                            </td>
+                                                            <td class="text-center">
+                                                                <button class="btn btn-primary btn-sm m-1" type="button">
+                                                                    <i class="bi bi-eye-fill"></i>
+                                                                </button>
+                                                                <button class="btn btn-success btn-sm m-1" type="button">
+                                                                    <i class="bi bi-reply-fill"></i>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div> <!-- /.card-body -->
                                         <div class="card-footer clearfix">
                                             <ul class="pagination pagination-sm m-0 float-end">
