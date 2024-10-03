c3f187c12e1a2dec5fa557e65827357f3ed292a6
MechaNeil <markneilteves12345@gmail.com>
2024-10-03 19:25:51 +0800

    add chart placeholder

---
+++ app/Livewire/Roles/Owner/Pages/Reports/Payment/PaymentListComponents/PaymentChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Roles\Owner\Pages\Reports\Payment\PaymentListComponents;
+
+use Livewire\Component;
+
+class PaymentChart extends Component
+{
+    public function render()
+    {
+        return view('livewire.roles.owner.pages.reports.payment.payment-list-components.payment-chart');
+    }
+}
